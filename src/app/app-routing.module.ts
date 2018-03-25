0bb5a63e9f068811acdbfda52caddbd73f586367
linzhengxing <zxtt456@163.com>
2018-03-25 21:04:49 +0800

    提交路由学习代码

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ProductComponent} from "./product/product.component";
 import {Code404Component} from "./code404/code404.component";
+import {ProductDescComponent} from "./product-desc/product-desc.component";
+import {SellerInfoComponent} from "./seller-info/seller-info.component";
 
 const routes: Routes = [
   {path: '', redirectTo:"/home", pathMatch:"full"},  //当URL路径为空时跳转到主页面 这是个重定向路由
   {path: 'home', component: HomeComponent},  //
-  {path: 'product', component: ProductComponent},
-  {path: 'product/:id', component: ProductComponent},
+  {path: 'product', component: ProductComponent,children:[
+    // {path:"",redirectTo:"/productDesc",pathMatch:"full"},
+    {path:"productDesc", component: ProductDescComponent},
+    {path:"", component: ProductDescComponent},
+    {path:"seller/:id", component: SellerInfoComponent}
+  ]},
+  {path: 'product/:id', component: ProductComponent,children:[
+    // {path:"",redirectTo:"/productDesc",pathMatch:"full"},
+    {path:"productDesc", component: ProductDescComponent},
+    {path:"", component: ProductDescComponent},
+    {path:"seller/:id", component: SellerInfoComponent}
+  ]},
   {path: '**', component: Code404Component},//当url与所有的路由都不匹配是跳转到这个组件
 ];
 
